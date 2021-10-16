4a47f1bc06e97c357eee0ddac4bfdc06dbb8534b
Tanishq-Jain <73748532+Tanishq-Jain@users.noreply.github.com>
2021-10-16 23:39:39 +0530

    Update Matrix_multiplication.py

--- Matrix_multiplication.py
+++ Matrix_multiplication.py
@@ -16,9 +16,9 @@ result = [[0,0,0,0],
 # iterate through rows of X
 for i in range(len(X)):
    # iterate through columns of Y
-   for j in range(len(Y[0])):
+for j in range(len(Y[0])):
        # iterate through rows of Y
-       for k in range(len(Y)):
+for k in range(len(Y)):
            result[i][j] += X[i][k] * Y[k][j]
 
 for r in result:
