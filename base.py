d11ed2e8e1460e01581d741532c15e501cb0a13d
Tanishq-Jain <73748532+Tanishq-Jain@users.noreply.github.com>
2021-10-16 23:36:44 +0530

    Update base.py

--- base.py
+++ base.py
@@ -5,6 +5,4 @@ c = float(input('Enter third side: '))
 # calculate the semi-perimeter
 s = (a + b + c) / 2
 
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
+# calculate the area--------area = (s*(s-a)*(s-b)*(s-c)) ** 0.5-------print('The area of the triangle is %0.2f' %area)
